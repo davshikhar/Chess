5f1b7af8ba61e896debd8501ab01a6c19656a7d3
davshikhar <shikharyadav900@gmail.com>
2025-08-17 23:03:21 +0530

    Initialised frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <button className='text-2xl font-semibold'>
+        Join
+      </button>
+    </div>
+  )
+}
+
+export default App
