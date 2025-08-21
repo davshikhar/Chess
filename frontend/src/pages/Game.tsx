d3bf835d4e20e4168e84a2fe0a3aa3e6de7dae69
davshikhar <shikharyadav900@gmail.com>
2025-08-21 22:47:56 +0530

    Added Button and ChessBoard component

--- frontend/src/pages/Game.tsx
+++ frontend/src/pages/Game.tsx
@@ -1,7 +1,20 @@
+import { Button } from "../components/Button"
+
 export const Game = () =>{
     return(
-        <div>
-            this is the game page!!
+        <div className="flex justify-center">
+            <div className="pt-8 max-w-screen-lg w-full">
+                <div className="grid grid-cols-6 gap-4 w-full">
+                    <div className="col-span-4 bg-red-200 w-full">
+                        <div className="col-span-4 bg-red-200 w-full">
+                            <ChessBoard/>
+                        </div>
+                        <div className="col-span-2 bg-green-200 w-full">
+                            <Button onClick={()=>Navigate("/game")}>Play Game</Button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import { Button } from "../components/Button";
 
 export const Landing = () =>{
     const navigate = useNavigate();
@@ -13,8 +14,7 @@ export const Landing = () =>{
                         <h1 className="text-4xl text-white font-bold mb-4">Welcome to the Chess Game</h1>
                         <p className="text-white text-lg mb-4 text-center">Click the button below to get started.</p>
                         <div className="mt-4 flex justify-center">
-                            <button onClick={() => navigate('/game')} 
-                            className="bg-green-500 text-white px-4 py-2 rounded hover:bg-blue-600 cursor-pointer transition duration-300">Start a game</button>
+                            <Button onClick={() => navigate('/game')}>Start a game</Button>
                         </div>
                     </div>
                 </div>
